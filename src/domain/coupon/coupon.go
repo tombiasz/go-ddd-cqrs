2258d565eec02e83d1df2d64a5471b5eb884cdfd
tombiasz <tomasz.m.nowacki@gmail.com>
2020-07-09 21:10:53 +0200

    add property getters

--- src/domain/coupon/coupon.go
+++ src/domain/coupon/coupon.go
@@ -1,13 +1,33 @@
 package coupon
 
 type coupon struct {
-	Id          string
-	Email       string
-	Code        string
-	Description string
-	Status      string
+	id          string
+	email       string
+	code        string
+	description string
+	status      string
 }
 
 func New(id, email, code, description, status string) *coupon {
 	return &coupon{id, email, code, description, status}
+}
+
+func (c coupon) Id() string {
+	return c.id
+}
+
+func (c coupon) Email() string {
+	return c.email
+}
+
+func (c coupon) Code() string {
+	return c.code
+}
+
+func (c coupon) Description() string {
+	return c.description
+}
+
+func (c coupon) Status() string {
+	return c.status
 }
--- src/main.go
+++ src/main.go
@@ -10,5 +10,9 @@ func main() {
 	c1 := coupon.New("1", "Foo", "Bar", "test", "test")
 
 	fmt.Println(c1)
-	fmt.Println(c1.Id)
+	fmt.Println(c1.Id())
+	fmt.Println(c1.Email())
+	fmt.Println(c1.Description())
+	fmt.Println(c1.Code())
+	fmt.Println(c1.Status())
 }
