8f003025f398e760012c8b305f2c62bc4dfe9778
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-04 21:45:35 +0200

    move shared test code to separate file

--- src/app/coupons/commands/register_coupon_test.go
+++ src/app/coupons/commands/register_coupon_test.go
@@ -8,24 +8,6 @@ import (
 	"time"
 )
 
-type FakeRepo struct {
-	onSave func(*coupon.Coupon) *domain.DomainError
-
-	onGetCouponByEmailAndCode func(*coupon.Email, *coupon.Code) (*coupon.Coupon, *domain.DomainError)
-
-	onGetExpiredCoupons func() ([]*coupon.Coupon, *domain.DomainError)
-}
-
-func (r *FakeRepo) Save(coupon *coupon.Coupon) *domain.DomainError {
-	return r.onSave(coupon)
-}
-
-func (r *FakeRepo) GetCouponByEmailAndCode(email *coupon.Email, code *coupon.Code) (*coupon.Coupon, *domain.DomainError) {
-	return r.onGetCouponByEmailAndCode(email, code)
-}
-func (r *FakeRepo) GetExpiredCoupons() ([]*coupon.Coupon, *domain.DomainError) {
-	return r.onGetExpiredCoupons()
-}
 func TestRegisterCouponCommandHandler(t *testing.T) {
 	var fakeNow = time.Now()
 	var fixedTimeProvider = &timeutils.FixedTimeProvider{fakeNow}
@@ -130,19 +112,4 @@ func TestRegisterCouponCommandHandler(t *testing.T) {
 			t.Errorf("expected %q error but did not received one", repoFailure)
 		}
 	})
-}
-
-func lookupError(t *testing.T, errs domain.DomainErrors, err error) bool {
-	t.Helper()
-
-	var found = false
-
-	for _, e := range errs {
-		if e == err {
-			found = true
-			break
-		}
-	}
-
-	return found
 }
---
+++ src/app/coupons/commands/testcommands.go
@@ -0,0 +1,41 @@
+package commands
+
+import (
+	"go-coupons/src/app/coupons/domain"
+	"go-coupons/src/app/coupons/domain/coupon"
+	"testing"
+)
+
+type FakeRepo struct {
+	onSave func(*coupon.Coupon) *domain.DomainError
+
+	onGetCouponByEmailAndCode func(*coupon.Email, *coupon.Code) (*coupon.Coupon, *domain.DomainError)
+
+	onGetExpiredCoupons func() ([]*coupon.Coupon, *domain.DomainError)
+}
+
+func (r *FakeRepo) Save(coupon *coupon.Coupon) *domain.DomainError {
+	return r.onSave(coupon)
+}
+
+func (r *FakeRepo) GetCouponByEmailAndCode(email *coupon.Email, code *coupon.Code) (*coupon.Coupon, *domain.DomainError) {
+	return r.onGetCouponByEmailAndCode(email, code)
+}
+func (r *FakeRepo) GetExpiredCoupons() ([]*coupon.Coupon, *domain.DomainError) {
+	return r.onGetExpiredCoupons()
+}
+
+func lookupError(t *testing.T, errs domain.DomainErrors, err error) bool {
+	t.Helper()
+
+	var found = false
+
+	for _, e := range errs {
+		if e == err {
+			found = true
+			break
+		}
+	}
+
+	return found
+}
