7a1e00c014b1552dffe91eeeb6bdaa0e9cd05318
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-08 18:22:58 +0200

    connect to postgres db

---
+++ src/db/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var out int64
+	err = conn.QueryRow(context.Background(), "select 1 + $1 as out", 42).Scan(&out)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(out)
+}
