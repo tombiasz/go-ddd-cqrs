03d175853485e903ae2840c992b27b0e9a7fc813
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-09 19:44:04 +0200

    query coupons from db

--- src/db/main.go
+++ src/db/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"go-coupons/src/app/coupons/domain"
+	"go-coupons/src/app/coupons/domain/coupon"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v4"
 )
@@ -17,12 +20,64 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	var out int64
-	err = conn.QueryRow(context.Background(), "select 1 + $1 as out", 42).Scan(&out)
+	rows, err := conn.Query(
+		context.Background(),
+		`select
+			id,  code, email, description, status, expdays, activatedAt, expiredAt, usedAt
+		from coupons`,
+	)
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(out)
+	for rows.Next() {
+
+		var couponIdRaw, codeRaw, emailRaw, descRaw, statusRaw string
+		var expdaysRaw uint8
+		var activatedAtRaw, expiredAtRaw, usedAtRaw *time.Time
+
+		err := rows.Scan(&couponIdRaw, &codeRaw, &emailRaw, &descRaw,
+			&statusRaw, &expdaysRaw, &activatedAtRaw, &expiredAtRaw, &usedAtRaw,
+		)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Scan failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		couponId, errId := coupon.NewCouponId(couponIdRaw)
+		code, errCode := coupon.NewCode(codeRaw)
+		email, errEmail := coupon.NewEmail(emailRaw)
+		desc, errDesc := coupon.NewDescription(descRaw)
+
+		errs := domain.CombineDomainErrors(errId, errCode, errEmail, errDesc)
+
+		if errs != nil {
+			fmt.Fprintf(os.Stderr, "Domain errors: %v\n", errs)
+			os.Exit(1)
+		}
+
+		var status coupon.Status
+		switch statusRaw {
+		case coupon.ActiveStatus:
+			status = coupon.NewActiveStatus(*activatedAtRaw, expdaysRaw)
+		case coupon.UsedStatus:
+			status = coupon.NewUsedStatus(*usedAtRaw)
+		case coupon.ExpiredStatus:
+			status = coupon.NewExpiredStatus(*expiredAtRaw)
+		}
+
+		coupon := coupon.New(
+			couponId,
+			email,
+			code,
+			desc,
+			status,
+		)
+
+		fmt.Printf("%s", coupon)
+	}
+
 }
