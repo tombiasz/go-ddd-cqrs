fcdd1407bdac276731ff83b3d109b577583accb2
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-13 22:24:42 +0200

    add GetCoupons endpoint

--- src/http/main.go
+++ src/http/main.go
@@ -5,6 +5,11 @@ import (
 	"go-coupons/src/http/middlewares"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"go-coupons/src/app/coupons/db"
+	"go-coupons/src/app/coupons/queries"
 
 	"github.com/go-chi/chi"
 )
@@ -23,6 +28,37 @@ func HandleIndex(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+type GetCouponsResponse struct {
+	Id          string     `json:"id"`
+	Code        string     `json:"code"`
+	Email       string     `json:"email"`
+	Description string     `json:"description"`
+	Status      string     `json:"status"`
+	Expdays     uint8      `json:"expiriesInDays"`
+	ActivatedAt time.Time  `json:"activatedAt"`
+	ExpiredAt   *time.Time `json:"expiredAt"`
+	UsedAt      *time.Time `json:"usedAt"`
+}
+
+func GetCouponsHandler(w http.ResponseWriter, _ *http.Request) {
+	handler := &queries.GetCouponsQueryHandler{
+		DbConnectionFactory: db.NewDbConnectionFactory(os.Getenv("DATABASE_URL")),
+	}
+
+	result := handler.Query()
+
+	var response []GetCouponsResponse
+	for _, r := range result {
+		response = append(response, GetCouponsResponse(r))
+	}
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middlewares.ContentTypeJson)
@@ -30,6 +66,7 @@ func NewRouter() *chi.Mux {
 	r.Route("/api/v1", func(r chi.Router) {
 
 		r.MethodFunc("GET", "/", HandleIndex)
+		r.MethodFunc("GET", "/coupons", GetCouponsHandler)
 	})
 
 	return r
