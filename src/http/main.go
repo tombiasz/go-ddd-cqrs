5a63f6c0f29fcb4341c37cf98736eaf179f0d56e
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-14 22:11:57 +0200

    add GetCouponById endpoint

--- src/http/main.go
+++ src/http/main.go
@@ -40,6 +40,18 @@ type GetCouponsResponse struct {
 	UsedAt      *time.Time `json:"usedAt"`
 }
 
+type GetCouponByIdResponse struct {
+	Id          string     `json:"id"`
+	Code        string     `json:"code"`
+	Email       string     `json:"email"`
+	Description string     `json:"description"`
+	Status      string     `json:"status"`
+	Expdays     uint8      `json:"expiriesInDays"`
+	ActivatedAt time.Time  `json:"activatedAt"`
+	ExpiredAt   *time.Time `json:"expiredAt"`
+	UsedAt      *time.Time `json:"usedAt"`
+}
+
 func GetCouponsHandler(w http.ResponseWriter, _ *http.Request) {
 	handler := &queries.GetCouponsQueryHandler{
 		DbConnectionFactory: db.NewDbConnectionFactory(os.Getenv("DATABASE_URL")),
@@ -59,6 +71,27 @@ func GetCouponsHandler(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func GetCouponByIdHandler(w http.ResponseWriter, r *http.Request) {
+	couponId := chi.URLParam(r, "couponId")
+
+	handler := &queries.GetCouponByIdQueryHandler{
+		DbConnectionFactory: db.NewDbConnectionFactory(os.Getenv("DATABASE_URL")),
+	}
+
+	result := handler.Query(couponId)
+
+	response := GetCouponByIdResponse(result)
+
+	// TODO:
+	// - handle 404
+	// handle other errors
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
 func NewRouter() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middlewares.ContentTypeJson)
@@ -67,6 +100,7 @@ func NewRouter() *chi.Mux {
 
 		r.MethodFunc("GET", "/", HandleIndex)
 		r.MethodFunc("GET", "/coupons", GetCouponsHandler)
+		r.MethodFunc("GET", "/coupons/{couponId}", GetCouponByIdHandler)
 	})
 
 	return r
