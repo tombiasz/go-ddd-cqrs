f8b2e5eaa8df9cc3a5537501c11db5960720894f
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-14 22:11:38 +0200

    introduce getCouponById query

---
+++ src/app/coupons/queries/get_coupon_by_id.go
@@ -0,0 +1,67 @@
+package queries
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go-coupons/src/app/coupons/db"
+)
+
+type GetCouponByIdQueryHandler struct {
+	DbConnectionFactory db.DbConnectionFactory
+}
+
+type GetCouponByIdQueryResult struct {
+	Id          string
+	Code        string
+	Email       string
+	Description string
+	Status      string
+	Expdays     uint8
+	ActivatedAt time.Time
+	ExpiredAt   *time.Time
+	UsedAt      *time.Time
+}
+
+func (h *GetCouponByIdQueryHandler) Query(couponId string) GetCouponByIdQueryResult {
+	con := h.DbConnectionFactory.GetConnection()
+	defer con.Close()
+
+	row := con.QueryRow(
+		context.Background(),
+		`SELECT id,
+       code,
+       email,
+       description,
+       status,
+       expdays,
+       activatedAt,
+       expiredAt,
+       usedAt
+			FROM coupons
+			WHERE id = $1
+		`,
+		couponId,
+	)
+
+	var r GetCouponByIdQueryResult
+
+	err := row.Scan(
+		&r.Id,
+		&r.Code,
+		&r.Email,
+		&r.Description,
+		&r.Status,
+		&r.Expdays,
+		&r.ActivatedAt,
+		&r.ExpiredAt,
+		&r.UsedAt,
+	)
+
+	if err != nil {
+		panic(fmt.Sprintf("GetCouponsQuery scan failed: %v\n", err))
+	}
+
+	return r
+}
