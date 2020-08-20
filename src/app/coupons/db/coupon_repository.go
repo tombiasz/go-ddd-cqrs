849962390d21ffa1ab0dcae15c11eadb6e5d5821
tombiasz <tomasz.m.nowacki@gmail.com>
2020-08-20 22:24:40 +0200

    fix Save; add GetExpiredCouons

--- src/app/coupons/db/coupon_repository.go
+++ src/app/coupons/db/coupon_repository.go
@@ -5,7 +5,21 @@ import (
 	"fmt"
 	"go-coupons/src/app/coupons/domain"
 	"go-coupons/src/app/coupons/domain/coupon"
+	"time"
 )
+
+// TODO: move to separate file
+type CouponModel struct {
+	Id          string
+	Code        string
+	Email       string
+	Description string
+	Status      string
+	Expdays     uint8
+	ActivatedAt time.Time
+	ExpiredAt   *time.Time
+	UsedAt      *time.Time
+}
 
 type couponRepository struct {
 	dbConnectionFactory DbConnectionFactory
@@ -30,7 +44,17 @@ func (r *couponRepository) Save(c *coupon.Coupon) *domain.DomainError {
 			activatedat,
 			expiredat,
 			usedat
-		) VALUES ($1, $2,	$3, $4, $5, $6, $7, $8, $9)`,
+		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+		ON CONFLICT (id) DO UPDATE SET
+			email = EXCLUDED.email,
+			code = EXCLUDED.code,
+			description = EXCLUDED.description,
+			status = EXCLUDED.status,
+			expdays = EXCLUDED.expdays,
+			activatedat = EXCLUDED.activatedat,
+			expiredat = EXCLUDED.expiredat,
+			usedat = EXCLUDED.usedat
+		`,
 		c.Id(),
 		c.Email(),
 		c.Code(),
@@ -54,5 +78,70 @@ func (r *couponRepository) GetCouponByEmailAndCode(*coupon.Email, *coupon.Code) 
 }
 
 func (r *couponRepository) GetExpiredCoupons() ([]*coupon.Coupon, *domain.DomainError) {
-	return nil, nil
+	con := r.dbConnectionFactory.GetConnection()
+
+	rows, err := con.Query(
+		context.Background(),
+		`SELECT
+			id,
+			email,
+			code,
+			description,
+			status,
+			expdays,
+			activatedat,
+			expiredat,
+			usedat
+		 FROM coupons
+			WHERE activatedat + interval '1 day' * expdays < now()
+				AND status = 'Active'
+		`,
+	)
+
+	if err != nil {
+		panic(fmt.Sprintf("GetExpiredCoupons query failed: %v\n", err))
+	}
+
+	var coupons []*coupon.Coupon
+
+	for rows.Next() {
+		var m CouponModel
+
+		err := rows.Scan(
+			&m.Id,
+			&m.Email,
+			&m.Code,
+			&m.Description,
+			&m.Status,
+			&m.Expdays,
+			&m.ActivatedAt,
+			&m.ExpiredAt,
+			&m.UsedAt,
+		)
+
+		if err != nil {
+			panic(fmt.Sprintf("GetExpiredCoupons scan failed: %v\n", err))
+		}
+
+		c, errDomain := coupon.New(
+			m.Id,
+			m.Email,
+			m.Code,
+			m.Description,
+			m.Status,
+			m.Expdays,
+			m.ActivatedAt,
+			m.ExpiredAt,
+			m.UsedAt,
+		)
+
+		if errDomain != nil {
+			panic(fmt.Sprintf("GetExpiredCoupons converting model to entity failed: %v\n", errDomain))
+		}
+
+		coupons = append(coupons, c)
+	}
+
+	// TODO: are two return values needed?
+	return coupons, nil
 }
